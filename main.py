304bb6d5b3d1157fcaa3c11840442f95f7364afb
canawa <ogut.mox.98@inbox.ru>
2025-07-04 18:28:26 +0300

    added extra

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 from fastapi import FastAPI
+from supabase import create_client, Client
+import dotenv
+import os
 
-app = FastAPI()
+dotenv.load_dotenv() # загружаем переменные окружения из .env файла (в проде не надо)
+url: str = os.environ.get("SUPABASE_URL") # надо будет добавить в ~bashrc на убунту серваке
+key: str = os.environ.get("SUPABASE_KEY") # надо будет добавить в ~bashrc на убунту серваке
+supabase: Client = create_client(url, key)
+app = FastAPI() 
+
+@app.get("/api/userInfo/{userName}") # динамический параметр, который будет передаваться в функцию. Потом сделаем так чтобы с админки можно было посмотреть информацию о всех пользователях
+def hello(userName: str):  # функция, которая будет возвращать информацию о пользователе
+    return {"message": f"Добро пожаловать, {userName}!",    
+     'userBalance': 'баланс пользователя (запрос supabase)',
+     'registrationDate': 'запрос регистрации (supabase)',
+     'cashoutAmount':'выведено в стим (запрос supabase)'} # возвращаемые данные
+
