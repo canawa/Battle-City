b757abd1434903a5c5c99ae801b6bb909b4d64ba
canawa <ogut.mox.98@inbox.ru>
2025-07-06 01:58:11 +0300

    added buttons and header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,12 @@
 <script setup> // можно просто писать js код, без экспорта
 import { ref } from 'vue' // ref - это функция из vue, которая позволяет создавать реактивные переменные (изменив их можно не перезагружать страницу чтобы увидеть результат) 
 
-const loginText = ref('Войти / Зарегистрироваться')
-const logo = ref('Battle City') // logo - это переменная, которая лежит в App.vue. Переменные из script передаются в template через {{ }}
 
-const loginButton = () => {
-  loginText.value = 'Влетаем в битву!' // когда оборачиваем переменную в ref, то мы можем использовать .value чтобы изменить ее значение, выглядит это так {value: 'наш текст'}, поэтому мы пишем loginText.value = 'наш текст'
-}
+const errorMessage = ref('')
 
-const loginProcess = async () => {
+
+
+const registerProcess = async () => {
   const login = document.getElementById('login-input').value
   const password = document.getElementById('password-input').value
   const response = await fetch('/api/register', {
@@ -20,50 +18,149 @@ const loginProcess = async () => {
   })
   const data = await response.json()
   console.log(data)
+  if (data.error == true) {
+    errorMessage.value = data.error
+  } else {
+    errorMessage.value = 'Успешно зарегистрированы! Подтвердите почту!'
+  }
+}
+
+const loginProcess = async () => {
+  const login = document.getElementById('login-input').value
+  const password = document.getElementById('password-input').value
+  const response = await fetch('/api/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ login, password })
+  })
+  const data = await response.json()
+  console.log(data)
+  if (data.error == true) {
+    errorMessage.value = data.error
+  } else {
+    errorMessage.value = 'Успешно вошли в аккаунт!'
+  }
 }
 
 </script>
 
 <template>
-  <div id="header">
+<div id="main-container">
 
-    <h1>{{ logo }}</h1> <!-- logo - это переменная, которая лежит в App.vue. Переменные из script передаются в template через {{ }} -->
-    <button type="button" id="login-button" @click = 'loginButton()' > {{ loginText }}  </button> <!-- в vue js юзаем @click и потом метод, функцию, которая будет выполняться при нажатии на кнопку-->
+  <div id="header">
+    <div id="header-logo">
+    
+      <img src="/src/assets/logo.png" alt="logo" id="logo"> <!-- logo - это переменная, которая лежит в App.vue. Переменные из script передаются в template через {{ }} -->
+    
+    </div>
+
+    <div id="header-auth-buttons">
+      <button type="button" id="header-login-button">  Sign in  </button> <!-- в vue js юзаем @click и потом метод, функцию, которая будет выполняться при нажатии на кнопку-->
+      <button type="button" id="header-register-button">  Create Account  </button> <!-- в vue js юзаем @click и потом метод, функцию, которая будет выполняться при нажатии на кнопку-->
+
+    </div>
+  </div>
+
+  <div id="register-form">
+    
+    <h2>Зарегистрироваться</h2>
+    <input type="email" id="login-input" placeholder="Почта">
+    <input type="password" id="password-input" placeholder="Пароль">
+    <button type="button" id="login-button" @click = 'registerProcess()' > Зарегистрироваться </button>
+  
   </div>
 
   <div id="login-form">
+    
+    <h2>Войти</h2>
     <input type="email" id="login-input" placeholder="Почта">
     <input type="password" id="password-input" placeholder="Пароль">
-    <button type="button" id="login-button" @click = 'loginProcess()' > Зарегистрироваться </button>
+    <button type="button" id="login-button" @click = 'loginProcess()' > Войти </button>
+  
+  </div>
+  
+  <div id="error-message">
+  
+    <p>{{ errorMessage }}</p>
+  
   </div>
 
+</div>
 </template>
 
 <style scoped> /* стили для всего что лежит в App.vue */
 
-#header { /* стили для div с id logo */
-  display: flex;
-  color: green;
-  justify-content: left;
-  font-size: 20px;
-  border: 1px solid black;
-  font-family: 'Press Start 2P', cursive;
+#logo {
+  width: 6%;
 }
 
-#login-button {
+#main-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: white;
+  height: 100vh;
+  padding: 0 1%;
+
+}
+
+
+
+#header { /* стили для div с id logo */
+  display: flex;
+  color: white;
+  width: 100%;
+  height: 8%;
+  font-family: 'Inter', sans-serif;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #12192b;
+  border-radius: 1rem;
+}
+
+#header-auth-buttons {
+  display: flex;
+  align-items: center;
+  justify-content: right;
+  font-size: 1.2rem;
+  margin-right: 1.5%;
+  width: 15%;
+  height: 100%;
+  /* border: 2px solid white; */
+}
+
+
+#header-register-button {
   display: flex;
   align-items: center;
   cursor: pointer;
+  color: white;
+  background-color: #09a24e;
   transition: background-color 0.3s ease;
+  border-radius: 10px;
+  height: 50%;
+  border: none;
+  
 }
 
-#login-button:hover {
-  background-color: #00ff00;
-  justify-content: right;
-  border: 1px solid black;
-  font-size: 20px;
-  font-family: 'Press Start 2P', cursive;
-  
+#header-register-button:hover {
+  background-color: #29ba67;
+}
+
+#header-login-button {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  color: white;
+  background-color: #1e2841;
+  transition: background-color 0.3s ease;
+  border-radius: 10px;
+  height: 50%;
+  padding: 0 4%;
+  margin-right: 2%;
+  border: none;
 }
 
 </style>
--- main.py
+++ main.py
@@ -1,21 +1,48 @@
 from fastapi import FastAPI
 from supabase import create_client, Client
 import dotenv
+from pydantic import BaseModel, Field, EmailStr
 import os
 
 dotenv.load_dotenv() # загружаем переменные окружения из .env файла (в проде не надо)
 url: str = os.environ.get("SUPABASE_URL") # надо будет добавить в ~bashrc на убунту серваке
-key: str = os.environ.get("SUPABASE_KEY") # надо будет добавить в ~bashrc на убунту серваке
-supabase: Client = create_client(url, key)
+service_key: str = os.environ.get("SUPABASE_SERVICE_ROLE_KEY") # надо будет добавить в ~bashrc на убунту серваке
+anon_key: str = os.environ.get("SUPABASE_ANON_KEY") # для регистрации пользователей используем ANON_KEY
+
+supabase_admin: Client = create_client(url, service_key)
+supabase_anon: Client = create_client(url, anon_key)
+
+
+
 app = FastAPI() 
 
-@app.get("/api/userInfo/{userName}") # динамический параметр, который будет передаваться в функцию. Потом сделаем так чтобы с админки можно было посмотреть информацию о всех пользователях
-def hello(userName: str):  # функция, которая будет возвращать информацию о пользователе
-    return {"message": f"Добро пожаловать, {userName}!",    
-     'userBalance': 'баланс пользователя (запрос supabase)',
-     'registrationDate': 'запрос регистрации (supabase)',
-     'cashoutAmount':'выведено в стим (запрос supabase)'} # возвращаемые данные
 
+class RegisterSchema(BaseModel):
+    login: EmailStr
+    password: str = Field(min_length=8, max_length=20)
 
+@app.post('/api/register')
+def register(user_data: RegisterSchema): #приходящий объект назовем user_data, и потом можно в него углубляться и вытаскивать данные
+    try:
+        response = supabase_anon.auth.sign_up( # используем метод (функцию) supabase.auth.sign_up для регистрации (от анонимного ключа, потому что от админки не идет письмо на почту)
+        {
+            'email': user_data.login, # передаем данные из объекта user_data в функцию supabase.auth.sign_up
+            'password': user_data.password # передаем данные из объекта user_data в функцию supabase.auth.sign_up
+        }
+        )
+        return response
+    except Exception as e:
+        return {'error': str(e)}
 
- 
+@app.post('/api/login')
+def login(user_data: RegisterSchema):
+    try:
+        response = supabase_anon.auth.sign_in_with_password(
+            {
+                'email': user_data.login,
+                'password': user_data.password
+            }
+        )
+        return response
+    except Exception as e:
+        return {'error': str(e)}
